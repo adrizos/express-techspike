908e0cfa320773e3c79f44ef62184a91a4b9e970
Alex Drizos <adrizosmpb@Alexanders-MBP.fios-router.home>
2018-10-21 20:42:35 -0400

    working

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var app = express();
 
 /* Middleware */
 
+
+
 // var logger = function(req, res, next){
 //   console.log('Logging...');
 //   next();
@@ -13,13 +15,55 @@ var app = express();
 // app.use(logger);
 
 
+/*
+Specify view engine
+run npm ejs --save from the command line.
+this will auto include ejs in the package.json file under dependencies
+*/
+app.set('view engine', 'ejs');
+
+/*
+next we'll specify what folder we want to use for views
+I've also created a /views directory in the root
+*/
+app.set('views', path.join(__dirname, 'views'));
+
 // body parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false})); //writing documentation for bodyParser
 
 
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+var users = [
+  {
+    id: 2,
+    firstName: 'Pete',
+    lastName: 'Davidson',
+    email: 'petepetepete@gmail.com'
+  },
+  {
+    id: 2,
+    firstName: 'Jenny',
+    lastName: 'Lewis',
+    email: 'rilokileysinger@yahoo.com'
+  },
+  {
+    id: 3,
+    firstName: 'Darby',
+    lastName: 'Crash',
+    email: 'thegerms@gmail.com'
+  }
+]
+
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  //var title = 'Customers';
+  res.render('index', {
+    title: 'Customers',
+    users: users 
+  });
 });
 
 app.listen(3000, function(){
